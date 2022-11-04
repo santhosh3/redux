06db5d23c47f1b08177c730b8d8709abb6ef046d
santhosh3 <santhoshchinna109@gmail.com>
2022-11-04 16:05:20 +0530

    redux basics

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {connect} from 'react-redux';
+import {InAction} from './Action';
+import {DecAction} from './Action'
 
-function App() {
+function App({local_varaiable,InAction,DecAction}) {
   return (
     <div>
-     <h1>Hello</h1>
+      <center>
+      <h1>{local_varaiable}</h1><br/>
+      <button onClick={InAction}>Increment</button>
+      <button onClick={DecAction}>Decrement</button>
+      </center>
     </div>
   )
 }
+const mapStateToProps = state => ({
+  local_varaiable : state
+})
 
-export default App
+export default connect(mapStateToProps,{InAction,DecAction})(App)
